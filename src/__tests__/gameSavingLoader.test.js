8b26d3e9e58badf7dba133b9fac551fe2bae41ac
Pavel Titov <otrepiev.grishka@yandex.ru>
2021-11-10 23:16:14 +0300

    промисы зло

--- src/__tests__/gameSavingLoader.test.js
+++ src/__tests__/gameSavingLoader.test.js
@@ -1,34 +1,8 @@
 import GameSavingLoader from "../gameSavingLoader";
 
 test(('GameSaving...'), () => {
-  const expected = {
-    "id": 22, // id сохранения
-    "created": 45656562, // timestamp создания
-    "userInfo": {
-      "id": 78, // user id
-      "name": 'kkkkk', // user name
-      "level": 456, // user level
-      "points": 9 // user points
-    }
-  };
-  let b = 25;
-  // b = GameSavingLoader.load().then((saving) => {
-  // saving объект класса GameSaving
-  //  expect(saving).toEqual(expected);
-  // console.log(saving);
-  //return saving}),
-  //(error) => {
-  // ...
-  //};
-  b = GameSavingLoader.load();
-  let c = b.then(res => c = res);
-  console.log(c);
-
-
-
-})
-
-          //let b;
-          //data.then().resolve(item => b = item);
-  // console.log(b);
-  //         });
+  const expected = '{"id":9,"created":1546300800,"userInfo":{"id":1,name":"Hitman","level":10,"points":2000}}';
+  return GameSavingLoader.load().then(result => {
+    expect(result).toEqual(expected);
+  })
+});
--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import GameSavingLoader from "./gameSavingLoader";
 
 GameSavingLoader.load().then((saving) => {
   // saving объект класса GameSaving
+  //const result = {id, created, userInfo} = saving;
+  //resolve(result);
 }, (error) => {
   // ...
 });
