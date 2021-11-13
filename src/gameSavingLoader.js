3a400f0db465c8255609a521f2a3965cd491c727
Pavel Titov <otrepiev.grishka@yandex.ru>
2021-11-14 02:21:41 +0300

    сделал цепочки then

--- src/gameSavingLoader.js
+++ src/gameSavingLoader.js
@@ -4,12 +4,9 @@ import json from './parser';
 export default class GameSavingLoader {
   static load(data) {
     const jsonData = JSON.stringify(data);
-    return new Promise((resolve) => {
-      read(jsonData).then((value) => {
-        json(value).then((result) => {
-          resolve(result);
-        });
-      });
+   return new Promise((resolve) => {
+      read(jsonData).then((value) => json(value))
+       .then(result => resolve(result));
     });
   }
 }
